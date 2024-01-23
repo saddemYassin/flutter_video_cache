29b9a420b0a271b8a8081a5847279e06d3b265b5
Yassin Saddem <yassin.saddam@t4ulabs.com>
2024-01-23 12:10:16 +0100

    feat: FlutterVideoCache init

--- lib/src/flutter_video_cache.dart
+++ lib/src/flutter_video_cache.dart
@@ -1,1 +1,21 @@
+import 'downloader_manager.dart';
 
+
+/// The plugin implementation for the flutter_video_cache plugin.
+class FlutterVideoCache {
+
+  /// True if packages has been initialized
+  static bool _isInitialized = false;
+
+  /// [_isInitialized] getter
+  bool get isInitialized => _isInitialized;
+
+
+  /// Initialize the package
+  static Future<void> init() async {
+    if(!_isInitialized) {
+      await DownloadManager.instance.init();
+      _isInitialized = true;
+    }
+  }
+}
